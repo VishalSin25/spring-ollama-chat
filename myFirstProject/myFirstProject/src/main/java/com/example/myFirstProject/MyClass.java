22837478e4dd63c2f74541660db0f847bdd6bf9d
vishalsingh <Vishal.Singh@iiitb.ac.in>
2025-06-08 14:41:14 +0530

    first commit

---
+++ myFirstProject/myFirstProject/src/main/java/com/example/myFirstProject/MyClass.java
@@ -0,0 +1,13 @@
+package com.example.myFirstProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyClass {
+
+    @GetMapping("abc")
+        public String HelloWorld(){
+            return "Hello";
+        }
+}
